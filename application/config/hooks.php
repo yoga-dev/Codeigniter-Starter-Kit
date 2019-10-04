c169ef85bb12246782dc2354e501c9f59ef3b7e0
yogeswaran <yogadev05@gmail.com>
2019-10-04 13:33:12 +0530

    whoops & vardumper packages added

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+$hook['pre_system'] = function () {
+
+    if (getenv('CI_ENV') == 'development') {
+        $whoops = new \Whoops\Run;
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+        $whoops->register();
+    }
+};
